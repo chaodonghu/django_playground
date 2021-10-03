0476d61723db06ce7d3523ae3c23d5ac4d9e2046
chaodonghu <dojostan@gmail.com>
2021-10-03 19:11:52 -0400

    add django admin

--- first_app/urls.py
+++ first_app/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from first_app import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('home/', views.home, name="home"),
-    path('educative/', views.educative, name="educative"),
-    path('<int:num>', views.even_or_odd, name='even_or_odd'),
+    path('', views.index,name="index"),
+    path('home/',views.home,name="home"),
+    path('educative/',views.educative,name="educative"),
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -1,29 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-
-
 def index(request):
-    my_dict = {'insert_me': "Hello I am from views.py!"}
-    return render(request, "first_app/index.html", context=my_dict)
-
+    return render(request,'first_app/index.html')
 
 def home(request):
     return HttpResponse("Welcome to home page!")
 
-
 def educative(request):
     return HttpResponse("Welcome to Educative page!")
-
-
-def show_age(request, age):
-    return HttpResponse("I am %s years old." % age)
-
-
-def even_or_odd(request, num):
-    if (num % 2 == 0):
-        output = "%s is an even number." % num
-    else:
-        output = "%s is an odd number." % num
-    return HttpResponse(output)
