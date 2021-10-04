57e548ff5c6d4cf75a823ebebcbeae1dd63d6c1b
chaodonghu <dojostan@gmail.com>
2021-10-03 21:40:20 -0400

    add many to many and many to one relationship on employees, company and project

--- first_app/models.py
+++ first_app/models.py
@@ -23,6 +23,32 @@ class Webpage(models.Model):
 class AccessRecord(models.Model):
     name = models.ForeignKey(Webpage, on_delete=models.CASCADE)
     date = models.DateField(default=date.today)
+    count = models.IntegerField(default=0)
 
     def __str__(self):
         return str(self.date)
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=264, unique=True)
+    number_of_employees = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    employee_name = models.CharField(max_length=264, unique=True)
+    company_name = models.ForeignKey(Company, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(default=date.today)
+
+    def __str__(self):
+        return self.employee_name
+
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=264, unique=True)
+    employee_name = models.ManyToManyField('Employee')
+
+    def __str__(self):
+        return self.project_name
